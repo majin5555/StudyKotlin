6a65ad5adf6bde075929869381adcfe75109996b
马晋 <13051152997@163.com>
2021-02-20 10:32:31 +0800

    初始化项目、

---
+++ app/src/main/java/cn/wlsk/kotlintest/MainActivity.kt
@@ -0,0 +1,20 @@
+package cn.wlsk.kotlintest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.longToast
+import org.jetbrains.anko.toast
+import java.util.logging.Logger
+import kotlin.math.log
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+ 
+    }
+
+
+}
